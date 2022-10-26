87bbec8fba06037b2b0575040e70e85d0867d7d0
Paúl Millanes <pauljmm99@gmail.com>
2022-10-25 22:12:33 -0700

    Visiting multiple pages in the spider

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -3,6 +3,8 @@ import scrapy
 # Titulo = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
 # Top ten tags = //div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()
+# Next page button = //ul[@class="pager"]/li[@class="next"]/a/@href
+
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
@@ -10,17 +12,45 @@ class QuotesSpider(scrapy.Spider):
         'https://quotes.toscrape.com/'
     ]
 
+    # Esto está deprecado
+    # custom_settings = {
+    #     'FEED_URI': 'quotes.json',
+    #     'FEED_FORMAT': 'json'
+    # }
+
+    custom_settings = {
+        'FEEDS': {
+            'quotes.json': {
+                'format': 'json',
+                'encoding': 'utf8',
+                'store_empty': False,
+                'fields': None,
+                'indent': 4,
+                'item_export_kwargs': {
+                    'export_empty_fields': True
+                }
+            }
+        }
+    }
+
     def parse(self, response):
-       
 
         title = response.xpath('//h1/a/text()').get()
-       
-        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-       
-        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()
+
+        quotes = response.xpath(
+            '//span[@class="text" and @itemprop="text"]/text()').getall()
+
+        top_ten_tags = response.xpath(
+            '//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()
 
         yield {
             'title': title,
             'quotes': quotes,
             'top_ten_tags': top_ten_tags
         }
+
+        next_page_button_link = response.xpath(
+            '//ul[@class="pager"]/li[@class="next"]/a/@href').get()
+
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, callback=self.parse)
